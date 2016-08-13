781f35f4703998aa4caf8010d441929d074d95e5
Federico Tomassetti <federico@tomassetti.me>
2016-08-13 13:33:52 +0200

    remove the transformation code

--- src/main/kotlin/me/tomassetti/sandy/sandy/ast/Model.kt
+++ src/main/kotlin/me/tomassetti/sandy/sandy/ast/Model.kt
@@ -1,58 +1,10 @@
 package me.tomassetti.sandy.sandy.ast
 
-import java.util.*
-import kotlin.reflect.KParameter
-import kotlin.reflect.memberProperties
-import kotlin.reflect.primaryConstructor
-
 //
 // Generic part: valid for all languages
 //
 
 interface Node
-
-fun Node.process(operation: (Node) -> Unit) {
-    operation(this)
-    this.javaClass.kotlin.memberProperties.forEach { p ->
-        val v = p.get(this)
-        when (v) {
-            is Node -> v.process(operation)
-            is Collection<*> -> v.forEach { if (it is Node) it.process(operation) }
-        }
-    }
-}
-
-fun Node.transform(operation: (Node) -> Node) : Node {
-    operation(this)
-    val changes = HashMap<String, Any>()
-    this.javaClass.kotlin.memberProperties.forEach { p ->
-        val v = p.get(this)
-        when (v) {
-            is Node -> {
-                val newValue = v.transform(operation)
-                if (newValue != v) changes[p.name] = newValue
-            }
-            is Collection<*> -> {
-                val newValue = v.map { if (it is Node) it.transform(operation) else it }
-                if (newValue != v) changes[p.name] = newValue
-            }
-        }
-    }
-    var instanceToTransform = this
-    if (!changes.isEmpty()) {
-        val constructor = this.javaClass.kotlin.primaryConstructor!!
-        val params = HashMap<KParameter, Any?>()
-        constructor.parameters.forEach { param ->
-            if (changes.containsKey(param.name)) {
-                params[param] = changes[param.name]
-            } else {
-                params[param] = this.javaClass.kotlin.memberProperties.find { param.name == it.name }!!.get(this)
-            }
-        }
-        instanceToTransform = constructor.callBy(params)
-    }
-    return operation(instanceToTransform)
-}
 
 //
 // Sandy specific part
