825ef3bc8c28d25825334a8f64425283188722f4
Federico Tomassetti <federico@tomassetti.me>
2016-08-13 16:26:16 +0200

    consider position in mapping

--- src/main/kotlin/me/tomassetti/sandy/sandy/ast/Model.kt
+++ src/main/kotlin/me/tomassetti/sandy/sandy/ast/Model.kt
@@ -4,13 +4,21 @@ package me.tomassetti.sandy.sandy.ast
 // Generic part: valid for all languages
 //
 
-interface Node
+interface Node {
+    val position: Position?
+}
+
+data class Point(val line: Int, val column: Int)
+
+data class Position(val start: Point, val end: Point)
+
+fun pos(startLine:Int, startCol:Int, endLine:Int, endCol:Int) = Position(Point(startLine,startCol),Point(endLine,endCol))
 
 //
 // Sandy specific part
 //
 
-data class SandyFile(val statements : List<Statement>) : Node
+data class SandyFile(val statements : List<Statement>, override val position: Position? = null) : Node
 
 interface Statement : Node { }
 
@@ -22,13 +30,9 @@ interface Type : Node { }
 // Types
 //
 
-object IntType : Type {
-    override fun toString() = "IntType"
-}
+data class IntType(override val position: Position? = null) : Type
 
-object DecimalType : Type {
-    override fun toString() = "DecimalType"
-}
+data class DecimalType(override val position: Position? = null) : Type
 
 //
 // Expressions
@@ -39,30 +43,30 @@ interface BinaryExpression : Expression {
     val right: Expression
 }
 
-data class SumExpression(override val left: Expression, override val right: Expression) : BinaryExpression
+data class SumExpression(override val left: Expression, override val right: Expression, override val position: Position? = null) : BinaryExpression
 
-data class SubtractionExpression(override val left: Expression, override val right: Expression) : BinaryExpression
+data class SubtractionExpression(override val left: Expression, override val right: Expression, override val position: Position? = null) : BinaryExpression
 
-data class MultiplicationExpression(override val left: Expression, override val right: Expression) : BinaryExpression
+data class MultiplicationExpression(override val left: Expression, override val right: Expression, override val position: Position? = null) : BinaryExpression
 
-data class DivisionExpression(override val left: Expression, override val right: Expression) : BinaryExpression
+data class DivisionExpression(override val left: Expression, override val right: Expression, override val position: Position? = null) : BinaryExpression
 
-data class UnaryMinusExpression(val value: Expression) : Expression
+data class UnaryMinusExpression(val value: Expression, override val position: Position? = null) : Expression
 
-data class TypeConversion(val value: Expression, val targetType: Type) : Expression
+data class TypeConversion(val value: Expression, val targetType: Type, override val position: Position? = null) : Expression
 
-data class VarReference(val varName: String) : Expression
+data class VarReference(val varName: String, override val position: Position? = null) : Expression
 
-data class IntLit(val value: String) : Expression
+data class IntLit(val value: String, override val position: Position? = null) : Expression
 
-data class DecLit(val value: String) : Expression
+data class DecLit(val value: String, override val position: Position? = null) : Expression
 
 //
 // Statements
 //
 
-data class VarDeclaration(val varName: String, val value: Expression) : Statement
+data class VarDeclaration(val varName: String, val value: Expression, override val position: Position? = null) : Statement
 
-data class Assignment(val varName: String, val value: Expression) : Statement
+data class Assignment(val varName: String, val value: Expression, override val position: Position? = null) : Statement
 
-data class Print(val value: Expression) : Statement
+data class Print(val value: Expression, override val position: Position? = null) : Statement
